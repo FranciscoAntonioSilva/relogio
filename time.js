fcd655dbe00a7c059d4e1ea10ff7a928ee3d0f79
Francisco Antonio <168096368+FranciscoAntonioSilva@users.noreply.github.com>
2025-06-16 09:52:50 -0300

    ajustando hora, minutos e segundos

--- time.js
+++ time.js
@@ -1,15 +1,22 @@
-let hoursText = document.getElementById('hora');
-let minutesText = document.getElementById('minutos');
-let secondsText = document.getElementById('segundos');
+let hours = document.getElementById('hora');
+let minutes = document.getElementById('minutos');
+let seconds = document.getElementById('segundos');
 
 const clockTime = () => {
     let hora = new Date().getHours();
     let minuto = new Date().getMinutes();
     let segundos = new Date().getSeconds();
 
-    hoursText.innerHTML = `${hora}`;
-    minutesText.innerHTML = `${minuto}`;
-    secondsText.innerHTML = `${segundos}`;
+    if(hora < 10)
+        hora = '0' + hora;
+    if(minuto < 10)
+        minuto = '0' + minuto;
+    if(segundos < 10)
+        segundos = '0' + segundos;
+
+    hours.innerHTML = `${hora}`;
+    minutes.innerHTML = `${minuto}`;
+    seconds.innerHTML = `${segundos}`;
 }
 
 setInterval(clockTime, 1000);
